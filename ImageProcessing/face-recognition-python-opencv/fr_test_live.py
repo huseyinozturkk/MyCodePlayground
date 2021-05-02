211868031f8db6b93b01ddc160309b080840da02
prometheus <ozturk.huse@protonmail.com>
2021-05-02 03:33:43 +0300

    Update fr_test_live.py

--- ImageProcessing/face-recognition-python-opencv/fr_test_live.py
+++ ImageProcessing/face-recognition-python-opencv/fr_test_live.py
@@ -1,1 +1,23 @@
 #! /usr/bin/python3
+
+import cv2,os
+import numpy as np
+import facerecognition as fr
+
+fr.FaceRecognitionInit()
+
+cam = cv2.VideoCapture(0)
+
+while True:
+
+    ret,frame = cam.read()
+    img = fr.FaceRecognition(frame,"video")
+
+    cv2.imshow("face recognition",img)
+
+    if cv2.waitKey(1) & 0xff == ord("q"):
+        break
+
+        
+cam.release()
+cv2.destroyAllWindows()
